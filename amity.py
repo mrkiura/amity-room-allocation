8fd41be6aeb83df63cea1a776f17ad7a917f9af2
Alex Kiura <alex.kiura@andela.com>
2016-03-09 07:59:18 +0300

    (Feature) Add methods to Amity class get_available_rooms, get_allocations

--- amity.py
+++ amity.py
@@ -9,13 +9,36 @@ class Amity:
     '''
 
     def __init__(self):
-        pass
+        self.rooms = []
 
     def allocate(self, people):
         '''
         Allocates spaces to the people included in the
         input file
         '''
+        self.allocations = []
         pass
 
-    d
+    def get_available_rooms(self):
+        '''
+        Returns a list of rooms with spaces
+        '''
+        available = []
+        for i in self.rooms:
+            if not i.is_full():
+                available.append(i)
+        return available
+
+    def get_allocations(self):
+        '''
+        Returns a list of rooms and the
+        people allocated to that room
+        '''
+        return self.allocations
+
+    def populate(self, rooms):
+        '''
+        Populates the building with 10 office
+        spaces and 10 living spaces from the input
+        file
+        '''
