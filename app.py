8adaeb8c923931b37a4df873b425d49169fae52a
Alex Kiura <alex.kiura@andela.com>
2016-03-11 09:37:28 +0300

    (Chore) Add Travis CI

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ def init_app():
 if __name__ == '__main__':
     premise = Amity()
     arguments = docopt(__doc__, version='Amity Room Allocator 0.0.1')
-    # print(arguments)
     if arguments['allocate']:
         premise.allocate()
 
@@ -32,25 +31,3 @@ if __name__ == '__main__':
 
     if arguments['see_allocations']:
         premise.print_allocations()
-
-# """Naval Fate.
-# Usage:
-#   naval_fate.py ship new <name>...
-#   naval_fate.py ship <name> move <x> <y> [--speed=<kn>]
-#   naval_fate.py ship shoot <x> <y>
-#   naval_fate.py mine (set|remove) <x> <y> [--moored|--drifting]
-#   naval_fate.py -h | --help
-#   naval_fate.py --version
-# Options:
-#   -h --help     Show this screen.
-#   --version     Show version.
-#   --speed=<kn>  Speed in knots [default: 10].
-#   --moored      Moored (anchored) mine.
-#   --drifting    Drifting mine.
-# """
-# from docopt import docopt
-
-
-# if __name__ == '__main__':
-#     arguments = docopt(__doc__, version='Naval Fate 2.0')
-#     print(arguments)
