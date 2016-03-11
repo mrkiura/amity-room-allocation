6cd5fdce7395a2a3f5db81f4decb2aa4d4d64881
Alex Kiura <alex.kiura@andela.com>
2016-03-11 17:43:43 +0300

    (Test) Add test to test_occupant

--- tests/test_occupants.py
+++ tests/test_occupants.py
@@ -5,10 +5,17 @@ from room import Room
 
 
 class OccupantsTest(unittest.TestCase):
+
     def setUp(self):
         self.occupant = Occupant(name='Alex Kiura', job_type='fellow')
 
     def test_occupant_get_job_type_result(self):
         result = self.occupant.get_job_type()
         self.assertEqual(result, 'fellow')
-        
+
+    def test_occupant_repr(self):
+        '''
+        Returns a string representation of the object
+        '''
+        result = self.occupant.__str__()
+        self.assertIsInstance(result, str)
